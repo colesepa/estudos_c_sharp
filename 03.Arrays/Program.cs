99150cdfadfba2816b6171cc5769e57603979a8b
Matheus Almeida <mjsa2005@hotmail.com>
2026-04-10 08:19:34 -0300

    arrays

---
+++ 03.Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+string[] orderId = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+List<string> suspectOrderId = [];
+
+foreach(var order in orderId)
+{
+    if (order.StartsWith('B'))
+    {
+        suspectOrderId.Add(order);
+    }
+}
+
+System.Console.WriteLine(string.Join("\n", suspectOrderId));
